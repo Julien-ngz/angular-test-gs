b4dbfe67e3cffe13901d6d529eb4bf37783977b6
Julien-ngz <julien.rodrigues.pers@gmail.com>
2021-08-01 17:19:05 +0200

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 /* . . . */
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
   items: Product[] = [];
   /* . . . */
-
+  constructor(private http: HttpClient) {}
   addToCart(product: Product) {
     this.items.push(product);
   }
@@ -19,4 +22,9 @@ export class CartService {
     return this.items;
   }
   /* . . . */
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
 }
